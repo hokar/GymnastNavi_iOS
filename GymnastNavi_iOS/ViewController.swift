03951a7df2e632a710d8ffa927a7c2619da08b7a
hookar <shunsuke886@gmail.com>
2016-04-11 02:01:24 +0900

    新規プロジェクト作成

---
+++ GymnastNavi_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GymnastNavi_iOS
+//
+//  Created by 帆苅 隼佑 on 2016/04/11.
+//  Copyright (c) 2016年 帆苅 隼佑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GymnastNavi_iOSTests/GymnastNavi_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  GymnastNavi_iOSTests.swift
+//  GymnastNavi_iOSTests
+//
+//  Created by 帆苅 隼佑 on 2016/04/11.
+//  Copyright (c) 2016年 帆苅 隼佑. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GymnastNavi_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
